8a8db68fce64fd9b5fca4861260612ae4b72e9b2
Bruno Randolf <br1@einfach.org>
2022-04-11 12:30:34 +0100

    Implement port.c and init dw3000

--- platform/port.c
+++ platform/port.c
@@ -1,21 +1,29 @@
+#include <zephyr.h>
+
+#include <deca_device_api.h>
+#include <dw3000.h>
 #include <port.h>
 
 void Sleep(uint32_t x)
 {
-	//deca_sleep(x);
+	k_msleep(x);
 }
 
 void reset_DWIC(void)
 {
-	// TODO
+	dw3000_spi_speed_slow();
+
+	dwt_softreset();
+
+	dw3000_spi_speed_fast();
 }
 
 void port_set_dw_ic_spi_slowrate(void)
 {
-	// TODO
+	dw3000_spi_speed_slow();
 }
 
 void port_set_dw_ic_spi_fastrate(void)
 {
-	// TODO
+	dw3000_spi_speed_fast();
 }
--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include <zephyr.h>
 #include <sys/printk.h>
 
+#include <dw3000.h>
 #include "../examples_info/examples_defines.h"
 
 extern example_ptr example_pointer;
@@ -19,6 +20,9 @@ void test_run_info(unsigned char *data)
 void main(void)
 {
 	printk("DW3000 Examples on %s\n", CONFIG_BOARD);
+
+	dw3000_init();
+	dw3000_hw_reset();
 
 	build_examples();
 	example_pointer();
